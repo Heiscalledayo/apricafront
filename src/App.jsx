cca8ebd8bb07a3caa4becab297fa12c92cd5641e
Awoyelevictor <awoyeleemma1@gmail.com>
2026-01-16 13:18:25 +0100

    victor's admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LandingPage } from './ui/LandingPage'
-import Navbar from './components/Home/Navbar' 
+import Navbar from './components/Home/Navbar'
+import AdminDashboard from './components/Admin/AdminDashboard'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      
+
       <Routes>
         <Route path='/' element={<LandingPage/>} />
+        <Route path='/admin' element={<AdminDashboard/>} />
       </Routes>
     </BrowserRouter>
   )
