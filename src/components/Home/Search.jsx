9c4b0247a0b6da5bfb651132f3d6f6a525cffd1b
Oduoye Ayomide <ayomidecoder@gmail.com>
2026-02-12 15:03:27 +0100

    OK

--- src/components/Home/Search.jsx
+++ src/components/Home/Search.jsx
@@ -80,7 +80,7 @@ const Search = () => {
             <option value="Tayo">Tayo (Male)</option>
           </select> 
           
-          {/* Custom Arrow Icon */}
+          {/* Custom Arrow Icon n*/}
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
             <svg
               className="fill-current h-4 w-4"
@@ -92,7 +92,7 @@ const Search = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div> 
   );
 };
 
